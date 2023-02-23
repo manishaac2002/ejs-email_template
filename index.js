d2a971c0a52dd82a917ddbc347afdbd137c75b8c
manishaac2002 <manishaac2002@gmail.com>
2023-02-23 16:52:06 +0530

    Updated-Template has created

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-var express = require('express');
-var app = express()
+// var express = require('express');
+// var app = express()
 
 
-app.get("/", function (req, res) {
-	let user = req.query.name
-	res.render("../views/hello.ejs", { emailtag:"Email confirmation", name:user,imglink:"https://randomuser.me/api/portraits/med/women/75.jpg", otptag :"Here is the ONE TIME PASSWORD",validitytag :"Valid for 30 minutes only...!",
-	para:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-	p: "We have sent email to manishaac2002@gmail.com to confirm the validity of our email address.After receicing the email follow the link provided to complete you registration ",Otp_1 :1,Otp_2 :2,Otp_3 :3,Otp_4 :4,Otp_5 :5,Otp_6 :6})
-	})
-// app.get("/head", function (req, res) {
-// 	res.render("../views/head.ejs", { headObject: "Ejs is fun" })
+// app.get("/", function (req, res) {
+// 	let user = req.query.name
+// 	res.render("../views/html.ejs", { 
+// 	emailtag:"Email confirmation",
+// 	name:user,imglink:"https://randomuser.me/api/portraits/med/women/75.jpg",
+// 	otptag :"Here is YOUR ONE TIME PASSWORD",
+// 	validitytag :"Valid for 30 minutes only...!",
+// 	para:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+// 	p: "We have sent email to manishaac2002@gmail.com to confirm the validity of our email address.After receicing the email follow the link provided to complete you registration ",
+// 	Otp_1 :1,Otp_2 :2,Otp_3 :3,Otp_4 :4,Otp_5 :5,Otp_6 :6})
+// 	})
+
+// app.listen(3000, function () {
+// 	console.log("server is listening!!!");
 // });
-app.listen(3000, function () {
-	console.log("server is listening!!!");
-});
-//<a href="https://www.flaticon.com/free-icons/email" title="email icons">Email icons created by Vectorslab - Flaticon</a>
+ejs.renderFile
+(
+	__dirname + "/test.ejs", { name: 'Stranger' }, function (err, data) 
+{
+	if (err) 
+	{
+		console.log(err);
+	} 
+	else 
+	{
+		var mainOptions = 
+		{
+			from: '"Tester" testmail@zoho.com',
+			to: "totest@zoho.com",
+			subject: 'Hello, world',
+			html: data
+		};
+		console.log("html data ======================>", mainOptions.html);
+		transporter.sendMail(mainOptions, function (err, info) 
+		{
+			if (err) 
+			{
+				console.log(err);
+			} 
+			else
+			 {
+				console.log('Message sent: ' + info.response);
+			}
+		});
+	}
+	
+	});
 
 
 // let me  = {
